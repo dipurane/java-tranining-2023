7a21fd7a1fbaeaf60bc1fab039b695ac5a545121
Empecial <115786659+EmUCN@users.noreply.github.com>
2023-04-17 12:58:03 +0200

    Added division method in Maths.java

--- CalculatorApp/src/Maths.java
+++ CalculatorApp/src/Maths.java
@@ -6,12 +6,18 @@ public class Maths {
 	public void add(int a,int b) {
 		
 		int result = a+b;
-		System.out.println("The Addtion is "+ result);
+		System.out.println("The Addtion is " + result);
 	}
 	
 	public void multiply(int a,int b) {
 		
 		int result = a*b;
-	    System.out.println("The multiplication is "+ result);
+	    System.out.println("The multiplication is " + result);
+	}
+
+	public void division(int a,int b) {
+		
+		int result = a/b;
+		System.out.println("The division is " + result);
 	}
 }
