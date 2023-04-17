67f20b07e854cdb2aa8dda3915235d9efcc6bed2
Dipesh <dipesh.rane@synerzip.com>
2023-04-17 15:23:59 +0530

    Calculator APP

---
+++ CalculatorApp/src/Entry.java
@@ -0,0 +1,14 @@
+
+public class Entry {
+
+	public static void main(String[] args) {
+	
+		int a = 10;
+		int b = 20;
+		Maths m  = new Maths();
+		m.add(a, b);
+		m.multiply(a, b);
+		
+		
+	}
+}
---
+++ CalculatorApp/src/Maths.java
@@ -0,0 +1,17 @@
+
+public class Maths {
+// Sub , Division , square, Cube
+	
+	
+	public void add(int a,int b) {
+		
+		int result = a+b;
+		System.out.println("The Addtion is "+ result);
+	}
+	
+	public void multiply(int a,int b) {
+		
+		int result = a*b;
+	    System.out.println("The multiplication is "+ result);
+	}
+}
